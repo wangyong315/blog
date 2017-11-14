05e1d103678adc80e6d21683d4717cbfdeac8864
Michael1935 <291140738@qq.com>
2017-11-14 23:28:13 +0800

    add package

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TodoHeaderContainer from '../../containers/TodoHeaderContainer';
-import TodoListContainer from '../../containers/TodoListContainer';
-
-const Main = () => (
-    <div>
-        <TodoHeaderContainers />
-        <TodoListContainer />
-    </div>
-);
-
-export default Main;
+export * from './Main';
